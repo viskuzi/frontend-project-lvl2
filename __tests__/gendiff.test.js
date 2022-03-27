88d14309f78b6a3b0d07bd920d7a6907739ccba5
Kanstantsin Lushchyk <viskuzi1987@gmail.com>
2022-03-27 13:14:28 +0200

    little improvment. 6 step

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,6 @@ import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import gendiff from '../src/genDiff.js';
-import stringify from '../src/final.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,14 +10,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing json files', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(readFile('expected_file.diff'));
+test('testing plain json files', () => {
+  expect(gendiff('plain_file1.json', 'plain_file2.json')).toEqual(readFile('expected_plain.diff'));
 });
 
 test('testing yaml files', () => {
   expect(gendiff('file1.yml', 'file2.yml')).toEqual(readFile('expected_file.diff'));
 });
 
-test('testing final json', () => {
-  expect(stringify('filepath1.json', 'filepath2.json')).toEqual(readFile('mainGendiff.diff'));
+test('testing complex json', () => {
+  expect(gendiff('file1.json', 'file2.json')).toEqual(readFile('expected_file.diff'));
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,35 +1,77 @@
+/* eslint-disable no-dupe-else-if */
+/* eslint-disable max-len */
 import _ from 'lodash';
 import parser from './parsers.js';
 
 const gendiff = (firstFileName, secondFileName) => {
-  const firstObj = parser(firstFileName);
-  const secondObj = parser(secondFileName);
+  const firstObject = parser(firstFileName);
+  const secondObject = parser(secondFileName);
 
-  const firstFileKeys = Object.keys(firstObj);
-  const secondFileKeys = Object.keys(secondObj);
-  const allKeys = _.union(firstFileKeys, secondFileKeys);
-  const allSortedKeys = _.sortBy(allKeys);
-  const commonProperties = {};
-  const PLUS_OPERATOR = '+';
-  const MINUS_OPERATOR = '-';
+  const iterPrint = (obj, depth = 1, replacer = '    ', replacersCount = 1) => {
+    const lines = [];
+    const fileKeys = Object.keys(obj);
+    const sortedKeys = _.sortBy(fileKeys);
 
-  allSortedKeys.forEach((key) => {
-    if (_.has(secondObj, key) && _.has(firstObj, key)) {
-      if (firstObj[key] === secondObj[key]) {
-        commonProperties[`  ${key}`] = firstObj[key];
+    sortedKeys.forEach((key) => {
+      if (_.isObject(obj[key])) {
+        const line = iterPrint(obj[key], depth + 1);
+        lines.push(`${replacer.repeat(replacersCount * depth)}${key}: ${line}`);
       } else {
-        commonProperties[`${MINUS_OPERATOR} ${key}`] = firstObj[key];
-        commonProperties[`${PLUS_OPERATOR} ${key}`] = secondObj[key];
+        lines.push(`${replacer.repeat(replacersCount * depth)}${key}: ${obj[key]}`);
       }
-    } else if (_.has(firstObj, key) && !_.has(secondObj, key)) {
-      commonProperties[`${MINUS_OPERATOR} ${key}`] = firstObj[key];
-    } else if (_.has(secondObj, key) && !_.has(firstObj, key)) {
-      commonProperties[`${PLUS_OPERATOR} ${key}`] = secondObj[key];
-    }
-  });
+    });
+    const shiftedLines = lines.map((line) => `\n${line}`);
+    return `{${shiftedLines.join('')}\n${replacer.repeat(replacersCount * depth - replacersCount)}}`;
+  };
+  const iter = (firstObj, secondObj, depth, replacer = '  ', replacersCount = 1) => {
+    const firstFileKeys = Object.keys(firstObj);
+    const secondFileKeys = Object.keys(secondObj);
+    const allKeys = _.union(firstFileKeys, secondFileKeys);
+    const allSortedKeys = _.sortBy(allKeys);
+    const PLUS_OPERATOR = '+';
+    const MINUS_OPERATOR = '-';
 
-  const result = JSON.stringify(commonProperties, null, 2);
-  return result.replace(/"/g, '').replace(/,/g, '');
+    const lines = [];
+    allSortedKeys.forEach((key) => {
+      if (_.has(secondObj, key) && _.has(firstObj, key) && _.isObject(firstObj[key]) && _.isObject(secondObj[key])) {
+        const line = iter(firstObj[key], secondObj[key], depth + 1);
+        lines.push(`${replacer.repeat(replacersCount * depth)}  ${key}: ${line}`);
+      } else if (_.has(secondObj, key) && _.has(firstObj, key) && !_.isObject(firstObj[key]) && !_.isObject(secondObj[key])) {
+        if (firstObj[key] === secondObj[key]) {
+          lines.push(`${replacer.repeat(replacersCount * depth)}  ${key}: ${firstObj[key]}`);
+        } else {
+          lines.push(`${replacer.repeat(replacersCount * depth)}${MINUS_OPERATOR} ${key}: ${firstObj[key]}`);
+          lines.push(`${replacer.repeat(replacersCount * depth)}${PLUS_OPERATOR} ${key}: ${secondObj[key]}`);
+        }
+      } else if (_.has(secondObj, key) && _.has(firstObj, key) && _.isObject(firstObj[key]) && !_.isObject(secondObj[key])) {
+        const line2 = iterPrint(firstObj[key], depth + 1);
+        lines.push(`${replacer.repeat(replacersCount * depth)}${MINUS_OPERATOR} ${key}: ${line2}`);
+        lines.push(`${replacer.repeat(replacersCount * depth)}${PLUS_OPERATOR} ${key}: ${secondObj[key]}`);
+      } else if (_.has(secondObj, key) && _.has(firstObj, key) && !_.isObject(firstObj[key]) && _.isObject(secondObj[key])) {
+        const line3 = iterPrint(secondObj[key], depth + 1);
+        lines.push(`${replacer.repeat(replacersCount * depth)}${MINUS_OPERATOR} ${key}: ${firstObj[key]}`);
+        lines.push(`${replacer.repeat(replacersCount * depth)}${PLUS_OPERATOR} ${key}: ${line3}`);
+      } else if (!_.has(secondObj, key) && _.has(firstObj, key)) {
+        if (_.isObject(firstObj[key])) {
+          const line4 = iterPrint(firstObj[key], depth + 1);
+          lines.push(`${replacer.repeat(replacersCount * depth)}${MINUS_OPERATOR} ${key}: ${line4}`);
+        } else {
+          lines.push(`${replacer.repeat(replacersCount * depth)}${MINUS_OPERATOR} ${key}: ${firstObj[key]}`);
+        }
+      } else if (_.has(secondObj, key) && !_.has(firstObj, key)) {
+        if (_.isObject(secondObj[key])) {
+          const line4 = iterPrint(secondObj[key], depth + 1);
+          lines.push(`${replacer.repeat(replacersCount * depth)}${PLUS_OPERATOR} ${key}: ${line4}`);
+        } else {
+          lines.push(`${replacer.repeat(replacersCount * depth)}${PLUS_OPERATOR} ${key}: ${secondObj[key]}`);
+        }
+      }
+    });
+    const shiftedLines = lines.map((line) => `\n${replacer.repeat(replacersCount * depth - replacersCount)}${line}`);
+    return `{${shiftedLines.join('')}\n${replacer.repeat(replacersCount * 2 * (depth - 1))}}`;
+  };
+
+  return iter(firstObject, secondObject, 1);
 };
 
 export default gendiff;
