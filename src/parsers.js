a1334e99b3269b6319bc5d69f1e4dc915838a90b
Kanstantsin Lushchyk <viskuzi1987@gmail.com>
2022-05-04 15:37:12 +0200

    fixing_bugs_1

--- src/parsers.js
+++ src/parsers.js
@@ -3,20 +3,20 @@ import path, { dirname } from 'path';
 import yaml from 'js-yaml';
 import { fileURLToPath } from 'url';
 
-// Function which retrieves JSON object given filename (entered by the user)
+// Function which retrieves JSON object by given filename (entered by the user)
 // - fileName, the name of the file entered by the user
 const getObjectByFilename = (fileName) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const pathToFile = path.join(__dirname, '..', '__fixtures__', fileName);
-
+  const yamlString = readFileSync(pathToFile);
   const extention = path.extname(fileName);
   switch (extention) {
     case '.json':
       return JSON.parse(readFileSync(pathToFile), 'utf-8');
     case '.yml':
     case '.yaml':
-      return yaml.load(readFileSync(pathToFile), 'utf-8');
+      return yaml.load(yamlString, 'utf-8');
     default:
       throw new Error(`Format '${extention}' is not supported.`);
   }
