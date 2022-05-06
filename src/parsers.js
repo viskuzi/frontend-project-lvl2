0d7e0aff1cec1cbf23c1976287d0c7ea10ccc6ff
Kanstantsin Lushchyk <viskuzi1987@gmail.com>
2022-05-06 21:58:02 +0200

    delete full filename resolution

--- src/parsers.js
+++ src/parsers.js
@@ -1,27 +1,18 @@
 import { readFileSync } from 'fs';
-import path, { dirname } from 'path';
+import path from 'path';
 import yaml from 'js-yaml';
-import { fileURLToPath } from 'url';
 
 // Function which retrieves JSON object by given filename (entered by the user)
-// - fileName, the name of the file entered by the user
+// - fileName, full path to fileName;
 const getObjectByFilename = (fileName) => {
-  console.log('fileName', fileName);
-  const __filename = fileURLToPath(import.meta.url);
-  console.log('__fileName', __filename);
-
-  const __dirname = dirname(__filename);
-  console.log('dirname', __dirname);
-  const pathToFile = path.join(__dirname, '..', '__fixtures__', fileName);
-  console.log('pathtofile', pathToFile);
-  const yamlString = readFileSync(pathToFile);
+  const fileBuffer = readFileSync(fileName);
   const extention = path.extname(fileName);
   switch (extention) {
     case '.json':
-      return JSON.parse(readFileSync(pathToFile), 'utf-8');
+      return JSON.parse(fileBuffer, 'utf-8');
     case '.yml':
     case '.yaml':
-      return yaml.load(yamlString, 'utf-8');
+      return yaml.load(fileBuffer, 'utf-8');
     default:
       throw new Error(`Format '${extention}' is not supported.`);
   }
