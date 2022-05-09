3cce50c1c2179411abcf5dad88666afc53e35584
Kanstantsin Lushchyk <viskuzi1987@gmail.com>
2022-05-09 14:37:47 +0200

    make functions unmutiable

--- formatters/capslockFormat.js
+++ formatters/capslockFormat.js
@@ -1,59 +1,56 @@
 import _ from 'lodash';
 
-const getComplexObjectStructure = (obj, DEPTH = 1, REPLACERs = '    ') => {
-  const lines = [];
+const getComplexObjectStructure = (obj, DEPTH = 1, REPLACER = '    ') => {
   const fileKeys = Object.keys(obj);
 
-  fileKeys.forEach((key) => {
+  const lines = fileKeys.map((key) => {
     if (_.isObject(obj[key])) {
       const line = getComplexObjectStructure(obj[key], DEPTH + 1);
-      lines.push(`${REPLACERs.repeat(DEPTH)}${key}: ${line}`);
-    } else {
-      lines.push(`${REPLACERs.repeat(DEPTH)}${key}: ${obj[key]}`);
+      return `${REPLACER.repeat(DEPTH)}${key}: ${line}`;
     }
+
+    return `${REPLACER.repeat(DEPTH)}${key}: ${obj[key]}`;
   });
   const shiftedLines = lines.map((line) => `\n${line}`);
-  return `{${shiftedLines.join('')}\n${REPLACERs.repeat(DEPTH - 1)}}`;
+  return `{${shiftedLines.join('')}\n${REPLACER.repeat(DEPTH - 1)}}`;
 };
-const capslock = (base, depth = 1, replacer = '  ') => {
+
+const stylish = (base, depth = 1, replacer = '  ') => {
   const PLUS_OPERATOR = '+';
   const MINUS_OPERATOR = '-';
-  const result = [];
 
-  base.forEach((element) => {
+  const lines = base.map((element) => {
     if (element.objectType === 'objDiff') {
-      const line1 = capslock(element.diffs, depth + 1);
-      result.push(`${replacer.repeat(depth * 2)}${element.key}: ${line1}`);
-    } else if (element.objectType === 'addedObject') {
+      const line1 = stylish(element.diffs, depth + 1);
+      return `${replacer.repeat(depth * 2)}${element.key}: ${line1}`;
+    } if (element.objectType === 'addedObject') {
       const line2 = getComplexObjectStructure(element.value, depth + 1);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${line2}`);
-    } else if (element.objectType === 'removedObject') {
+      return `${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${line2}`;
+    } if (element.objectType === 'removedObject') {
       const line3 = getComplexObjectStructure(element.value, depth + 1);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${line3}`);
-    } else if (element.objectType === 'addedValue') {
-      result.push(`${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.value}`);
-    } else if (element.objectType === 'removedValue') {
-      result.push(`${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.value}`);
-    } else if (element.objectType === 'objectToValue') {
+      return `${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${line3}`;
+    } if (element.objectType === 'addedValue') {
+      return `${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.value}`;
+    } if (element.objectType === 'removedValue') {
+      return `${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.value}`;
+    } if (element.objectType === 'objectToValue') {
       const line4 = getComplexObjectStructure(element.objectValue, depth + 1);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${line4}`);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.plainValue}`);
-    } else if (element.objectType === 'valueToObject') {
+      return `${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${line4}\n${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.plainValue}`;
+    } if (element.objectType === 'valueToObject') {
       const line5 = getComplexObjectStructure(element.objectValue, depth + 1);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.plainValue}`);
-      result.push(`${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${line5}`);
-    } else if (element.objectType === 'valueToValue') {
+      return `${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.plainValue}\n${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${line5}`;
+    } if (element.objectType === 'valueToValue') {
       if (element.oldValue === element.newValue) {
-        result.push(`${replacer.repeat(depth * 2)}${element.key}: ${element.oldValue}`);
-      } else {
-        result.push(`${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.oldValue}`);
-        result.push(`${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.newValue}`);
+        return `${replacer.repeat(depth * 2)}${element.key}: ${element.oldValue}`;
       }
+      return `${replacer.repeat(depth * 2 - 1)}${MINUS_OPERATOR} ${element.key}: ${element.oldValue}\n${replacer.repeat(depth * 2 - 1)}${PLUS_OPERATOR} ${element.key}: ${element.newValue}`;
     }
+
+    return base;
   });
-  const shiftedLines = result.map((res) => `\n${res.toUpperCase()}`);
+  const shiftedLines = lines.map((res) => `\n${res.toUpperCase()}`);
 
   return `{${shiftedLines.join('')}\n${replacer.repeat((depth - 1) * 2)}}`;
 };
 
-export default capslock;
+export default stylish;
