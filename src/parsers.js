83adac8c9f9e4f662d87622e62929a24e7babb0a
Kanstantsin Lushchyk <viskuzi1987@gmail.com>
2022-05-10 11:49:36 +0200

    fix problem with code repeat

--- src/parsers.js
+++ src/parsers.js
@@ -5,37 +5,30 @@ import { fileURLToPath } from 'url';
 
 // Function which retrieves JSON object by given filename (entered by the user)
 // - fileName, full path to fileName;
+const detectFileNamePath = (filename) => {
+  const handledFileName = filename.split('/');
+  if (handledFileName.length > 1) {
+    return readFileSync(filename);
+  }
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFile = path.join(__dirname, '..', '__fixtures__', filename);
+  return readFileSync(pathToFile);
+};
 
 const getObjectByFilename = (fileName) => {
-  const handledFileName = fileName.split('/');
   const extention = path.extname(fileName);
+  const fileBuffer = detectFileNamePath(fileName);
 
-  if (handledFileName.length > 1) {
-    const fileBuffer = readFileSync(fileName);
-    switch (extention) {
-      case '.json':
-        return JSON.parse(fileBuffer, 'utf-8');
-      case '.yml':
-      case '.yaml':
-        return yaml.load(fileBuffer, 'utf-8');
-      default:
-        throw new Error(`Format '${extention}' is not supported.`);
-    }
-  } else {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const pathToFile = path.join(__dirname, '..', '__fixtures__', fileName);
-    switch (extention) {
-      case '.json':
-        return JSON.parse(readFileSync(pathToFile), 'utf-8');
-      case '.yml':
-      case '.yaml':
-        return yaml.load(readFileSync(pathToFile), 'utf-8');
-      default:
-        throw new Error(`Format '${extention}' is not supported.`);
-    }
+  switch (extention) {
+    case '.json':
+      return JSON.parse(fileBuffer, 'utf-8');
+    case '.yml':
+    case '.yaml':
+      return yaml.load(fileBuffer, 'utf-8');
+    default:
+      throw new Error(`Format '${extention}' is not supported.`);
   }
 };
 
-getObjectByFilename('file1.json');
 export default getObjectByFilename;
